0001857f454fd6aaa1a49117c188c8275dfbc902
Montana Mendy <montana@linux.com>
2022-06-06 05:34:57 -0700

    change where notes are placed

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import { FooProvider } from "./foo-context.js";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+// Hypothetical App.js file existing, I've only laid out the React skeleton for this.
+
 root.render(
   <StrictMode>
-    <App /> // Hypothetical App.js file existing, I've only laid out the React skeleton for this.
+    <App />
   </StrictMode>
 );
